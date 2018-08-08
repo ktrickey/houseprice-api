d0d4419059e70c46095bcdb3b865af3688fb2585
Kevin Trickey <registrations@kevintrickey.me.uk>
2018-08-08 23:47:26 +0100

    Import all 23million + records...

---
+++ src/HousePrice.Api/HousePrice.Api.Cli/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using HousePrice.Api.Services;
+
+namespace HousePrice.Api.Cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Populating lookup");
+        //    PostcodeLookup.GetByPostcode("");//force population of lookup
+            var importer = new HousePrice.Api.Services.Importer();
+//            var tasks = new List<Task>();
+//;            for (int year = 1995; year <= 2018; year++)
+//            {
+//                Console.WriteLine($"Importing {year}");
+//                var filename = $@"C:\Dev\HousePrice\data\{year}.csv";
+//                tasks.Add(Task.Run(()=>importer.Import(new FileStream(filename,
+//                    FileMode.Open, FileAccess.Read))));
+//            }
+//
+        //   Task.WaitAll(tasks.ToArray());
+           Console.WriteLine("Adding index");
+           await importer.AddIndex();
+        }
+    }
+}
---
+++ src/HousePrice.Api/HousePrice.Api.Services/Importer.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace HousePrice.Api.Services
+{
+    public static class PostcodeLookup
+    {
+        [Serializable]
+        public class PostcodeData
+        {
+            private string _postcode;
+
+            public long Id { get; set; }
+
+            public string Postcode
+            {
+                get => _postcode;
+                set => _postcode = value.Replace(" ", string.Empty);
+            }
+
+            public double? Latitude { get; set; }
+            public double? Longitude { get; set; }
+        }
+
+        private static Dictionary<string, PostcodeData> postcodeLookup;
+        static PostcodeLookup()
+        {
+            using (var streamReader = new StreamReader(new FileStream(@"c:\mongo\ukpostcodes.csv", FileMode.Open, FileAccess.Read)))
+            {
+                using (var csvReader = new CsvHelper.CsvReader(streamReader))
+                {
+                    csvReader.Configuration.BufferSize = 65536;
+                    var recs = csvReader.GetRecords<PostcodeData>();
+                    postcodeLookup = recs.ToDictionary(p => p.Postcode);
+  
+                }
+            }
+        }
+
+        public static PostcodeData GetByPostcode(string postcode)
+        {
+            var found = postcodeLookup.TryGetValue(postcode, out PostcodeData postcodeData);
+            return found? postcodeData:null;
+        }
+    }
+
+    public class Location
+    {
+        private readonly double? _latitude;
+        private readonly double? _longitude;
+        private readonly bool _isValid;
+
+        public Location(double? latitude, double? longitude)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+            _isValid = _latitude.HasValue && _longitude.HasValue;
+        }
+        [BsonConstructor]
+        public Location(double[] coordinates): this(coordinates[1], coordinates[0])
+        {
+            
+        }
+
+        // ReSharper disable once PossibleInvalidOperationException
+        // ReSharper disable once PossibleInvalidOperationException
+        [BsonElement("coordinates")]
+        public double[] Coordinates =>  _isValid? new double[] {_longitude.Value, _latitude.Value}:null;
+        [BsonElement("type")]
+        public string Type => "Point";
+
+    }
+    internal class HousePrice
+    {
+        [BsonId]
+        public string TransactionId { get; set; }
+        public decimal Price { get; set; }
+        public DateTime TransferDate { get; set; }
+        public string Postcode { get; set; }
+        public string PropertyType { get; set; }
+        public string IsNew { get; set; }
+        public string Duration { get; set; }
+        public string PAON { get; set; }
+        public string SAON { get; set; }
+        public string Street { get; set; }
+        public string Locality { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string County { get; set; }
+        public string CategoryType { get; set; }
+        public string Status { get; set; }
+        public Location Location { get; set; }
+    }
+    
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal sealed class HousePriceMap : ClassMap<HousePrice>
+    {
+        public HousePriceMap()
+        {
+            Map( m => m.TransactionId ).Index(0);
+            Map( m => m.Price ).Index(1);
+            Map( m => m.TransferDate ).Index(2);
+            Map( m => m.Postcode ).Index(3);
+            Map( m => m.PropertyType ).Index(4);
+            Map( m => m.IsNew ).Index(5);
+            Map( m => m.Duration ).Index(6);
+            Map( m => m.PAON ).Index(7);
+            Map( m => m.SAON ).Index(8);
+            Map( m => m.Street ).Index(9);
+            Map( m => m.Locality ).Index(10);
+            Map( m => m.City ).Index(11);
+            Map( m => m.District ).Index(12);
+            Map( m => m.County ).Index(13);
+            Map( m => m.CategoryType ).Index(14);
+            Map( m => m.Status ).Index(15);
+        }
+    }
+    public class Importer
+    {
+        public async Task Import(Stream csvStream)
+        {
+            using (var streamReader = new StreamReader(csvStream))
+            {
+                using (var csvReader = new CsvHelper.CsvReader(streamReader))
+                {
+
+                    csvReader.Configuration.HasHeaderRecord = false;
+                    csvReader.Configuration.RegisterClassMap<HousePriceMap>();
+                    var client = new MongoClient("mongodb://localhost:32773");
+                    var database = client.GetDatabase("HousePrice");
+               //     database.DropCollection("Transactions");
+                    var collection = database.GetCollection<HousePrice>("Transactions");
+			
+                    var batch = new List<HousePrice>();
+
+                    while (csvReader.Read())
+                    {
+                        var record = csvReader.GetRecord<HousePrice>();
+                        record.Postcode = record.Postcode.Replace(" ", String.Empty);
+                        var locationData = PostcodeLookup.GetByPostcode(record.Postcode);
+                        record.Location = locationData?.Latitude!=null && locationData?.Longitude!=null?new Location(locationData?.Latitude, locationData?.Longitude): null;
+                        batch.Add(record);
+                        
+                        if (batch.Count == 1000)
+                        {
+                            await collection.InsertManyAsync(batch);
+                            batch.Clear();
+                        }
+
+          
+                    }
+                    await collection.InsertManyAsync(batch);
+
+                                     
+//                    
+//                    var housePriceBuilder = Builders<HousePrice>.IndexKeys;
+//                    var indexModel = new CreateIndexModel<HousePrice>(housePriceBuilder.Geo2DSphere(x=>x.Location));
+//                    await collection.Indexes.CreateOneAsync(indexModel).ConfigureAwait(false);
+   
+                }
+            }
+        }
+
+        public async Task AddIndex()
+        {
+            var client = new MongoClient("mongodb://localhost:32768");
+            var database = client.GetDatabase("HousePrice");
+            //     database.DropCollection("Transactions");
+            var collection = database.GetCollection<HousePrice>("Transactions");
+
+                                var housePriceBuilder = Builders<HousePrice>.IndexKeys;
+                    var indexModel = new CreateIndexModel<HousePrice>(housePriceBuilder.Geo2DSphere(x=>x.Location));
+                    await collection.Indexes.CreateOneAsync(indexModel).ConfigureAwait(false);
+        }
+        
+    }
+}
