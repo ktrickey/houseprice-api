93a5c469a3f184a1fc446639ccb8d00f9cd2926c
kevin.trickey <kevin.trickey@ul.com>
2019-06-07 17:04:01 +0100

    Add the tests project

---
+++ tests/CleanArchitecture.Tests/NoOpDomainEventDispatcher.cs
@@ -0,0 +1,10 @@
+﻿using HousePrice.Api.Core.Interfaces;
+using HousePrice.Api.Core.SharedKernel;
+
+namespace CleanArchitecture.Tests
+{
+    public class NoOpDomainEventDispatcher : IDomainEventDispatcher
+    {
+        public void Dispatch(BaseDomainEvent domainEvent) { }
+    }
+}
